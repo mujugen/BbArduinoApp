c39826c9cb96034a378f0d520cbb1383b680897f
mujugen <101939481+mujugen@users.noreply.github.com>
2023-10-22 09:54:48 +0800

    working data saving/loading

--- ArduinoApp.ino
+++ ArduinoApp.ino
@@ -12,7 +12,11 @@
 SoftwareSerial mySerial(Finger_Rx, Finger_Tx);
 Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
 
-const int maxIds = 128; // Maximum number of fingerprint IDs
+uint8_t f_buf[512];
+
+const int maxIds = 10;
+uint8_t fingerTemplate[512];
+uint8_t allTemplates[maxIds][512];
 bool enrolledIds[maxIds] = {false}; // Array to keep track of enrolled IDs
 
 const char* ssid = "LHWifi";
@@ -33,7 +37,7 @@ void setup() {
 
 
   // Set the data rate for the sensor serial port
-  finger.begin(57600);
+  finger.begin(9600);
 
   if (finger.verifyPassword()) {
     Serial.println("Found fingerprint sensor!");
@@ -96,7 +100,7 @@ uint8_t readnumber(void) {
   uint8_t num = 0;
 
   while (num == 0) {
-    while (!Serial.available());
+    while (! Serial.available());
     num = Serial.parseInt();
   }
   return num;
@@ -140,6 +144,32 @@ void loop() {
       Serial.println("Sending to api endpoint");
       sendMessageToAPI();
     }   
+    else if (cmd == '3') {
+      Serial.println("deleting all templates");
+      deleteAllTemplates();
+    }  
+    else if (cmd == '2') {
+      Serial.println("storing all templates");
+      storeAllTemplates();
+    }  
+    else if (cmd == '1') {
+      Serial.println("downloading all templates");
+      downloadAllTemplates();
+    }
+    else if (cmd == '5') {
+      Serial.println("store_template_to_buf");
+      store_template_to_buf();
+    }
+    else if (cmd == '6') {
+      Serial.println("show_from_saved");
+      for (uint8_t id = 1; id <= 10; id++) {        
+      show_from_saved(id);
+      }
+    }
+    else if (cmd == '7') {
+      Serial.println("write_template_data_to_sensor");
+      write_template_data_to_sensor();
+    }
   } 
 
   webServer.handleClient();
@@ -440,4 +470,252 @@ void sendMessageToAPI() {
 void handleAPITest() {
   Serial.println("received request");
   webServer.send(204);  // Send an empty response with 204 No Content status
+}
+
+
+
+void deleteAllTemplates() {
+  for (uint8_t id = 1; id <= maxIds; id++) {
+    deleteFingerprint(id);
+  }
+  Serial.println("All templates deleted");
+}
+
+void downloadAllTemplates(){
+  for (int finger = 1; finger < 10; finger++) {
+    downloadFingerprintTemplate(finger);
+  }
+}
+
+void storeAllTemplates() {  // Assuming each template is 512 bytes
+  for (uint8_t id = 1; id <= maxIds; id++) {
+    downloadFingerprintTemplate(id);
+    // Assuming downloadFingerprintTemplate stores the template in a global variable named fingerTemplate
+    memcpy(allTemplates[id - 1], fingerTemplate, 512);
+  }
+  // Now allTemplates contains all the fingerprint templates from the sensor
+  // You can send them to a server, or store them in non-volatile memory for later retrieval
+}
+
+uint8_t downloadFingerprintTemplate(uint16_t id)
+{
+  Serial.println("------------------------------------");
+  Serial.print("Attempting to load #"); Serial.println(id);
+  uint8_t p = finger.loadModel(id);
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.print("Template "); Serial.print(id); Serial.println(" loaded");
+      break;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    default:
+      Serial.print("Unknown error "); Serial.println(p);
+      return p;
+  }
+
+  // OK success!
+
+  Serial.print("Attempting to get #"); Serial.println(id);
+  p = finger.getModel();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.print("Template "); Serial.print(id); Serial.println(" transferring:");
+      break;
+    default:
+      Serial.print("Unknown error "); Serial.println(p);
+      return p;
+  }
+
+  // one data packet is 267 bytes. in one data packet, 11 bytes are 'usesless' :D
+  uint8_t bytesReceived[534]; // 2 data packets
+  memset(bytesReceived, 0xff, 534);
+
+  uint32_t starttime = millis();
+  int i = 0;
+  while (i < 534 && (millis() - starttime) < 20000) {
+    if (mySerial.available()) {
+      bytesReceived[i++] = mySerial.read();
+    }
+  }
+  Serial.print(i); Serial.println(" bytes read.");
+  Serial.println("Decoding packet...");
+
+  uint8_t fingerTemplate[512]; // the real template
+  memset(fingerTemplate, 0xff, 512);
+
+  // filtering only the data packets
+  int uindx = 9, index = 0;
+  memcpy(fingerTemplate + index, bytesReceived + uindx, 256);   // first 256 bytes
+  uindx += 256;       // skip data
+  uindx += 2;         // skip checksum
+  uindx += 9;         // skip next header
+  index += 256;       // advance pointer
+  memcpy(fingerTemplate + index, bytesReceived + uindx, 256);   // second 256 bytes
+
+  for (int i = 0; i < 512; ++i) {
+    //Serial.print("0x");
+    printHex(fingerTemplate[i], 2);
+    //Serial.print(", ");
+  }
+  Serial.println("\ndone.");
+
+  return p;
+}
+
+
+
+void printHex(int num, int precision) {
+  char tmp[16];
+  char format[128];
+
+  sprintf(format, "%%.%dX", precision);
+
+  sprintf(tmp, format, num);
+  Serial.print(tmp);
+}
+
+
+void store_template_to_buf(){
+
+  Serial.println("Waiting for valid finger....");
+  while (finger.getImage() != FINGERPRINT_OK) { // press down a finger take 1st image 
+  }
+  Serial.println("Image taken");
+
+
+  if (finger.image2Tz(1) == FINGERPRINT_OK) { //creating the charecter file for 1st image 
+    Serial.println("Image converted");
+  } else {
+    Serial.println("Conversion error");
+    return;
+  }
+
+  Serial.println("Remove finger");
+  delay(2000);
+  uint8_t p = 0;
+  while (p != FINGERPRINT_NOFINGER) {
+    p = finger.getImage();
+  }
+
+  Serial.println("Place same finger again, waiting....");
+  while (finger.getImage() != FINGERPRINT_OK) { // press the same finger again to take 2nd image
+  }
+  Serial.println("Image taken");
+
+
+  if (finger.image2Tz(2) == FINGERPRINT_OK) { //creating the charecter file for 2nd image 
+    Serial.println("Image converted");
+  } else {
+    Serial.println("Conversion error");
+    return;
+  }
+
+
+  Serial.println("Creating model...");
+
+  if (finger.createModel() == FINGERPRINT_OK) {  //creating the template from the 2 charecter files and saving it to char buffer 1
+    Serial.println("Prints matched!");
+    Serial.println("Template created");
+  } else {
+    Serial.println("Template not build");
+    return;
+  }
+
+  Serial.println("Attempting to get template..."); 
+  if (finger.getModel() == FINGERPRINT_OK) {  //requesting sensor to transfer the template data to upper computer (this microcontroller)
+    Serial.println("Transferring Template...."); 
+  } else {
+    Serial.println("Failed to transfer template");
+    return;
+  }
+  
+  if (finger.get_template_buffer(512, f_buf) == FINGERPRINT_OK) { //read the template data from sensor and save it to buffer f_buf
+    Serial.println("Template data (comma sperated HEX):");
+    for (int k = 0; k < (512/finger.packet_len); k++) { //printing out the template data in seperate rows, where row-length = packet_length
+      for (int l = 0; l < finger.packet_len; l++) {
+        Serial.print("0x");
+        Serial.print(f_buf[(k * finger.packet_len) + l], HEX);
+        Serial.print(",");
+      }
+      Serial.println("");
+    }
+  }
+
+}
+
+void show_from_saved(uint16_t id) {
+  Serial.println("------------------------------------");
+  Serial.print("Attempting to load #"); Serial.println(id);
+  uint8_t p = finger.loadModel(id);
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.print("Template "); Serial.print(id); Serial.println(" loaded");
+      break;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return;
+    default:
+      Serial.print("Unknown error "); Serial.println(p);
+      return ;
+  }
+
+  // OK success!
+
+  Serial.print("Attempting to get #"); Serial.println(id);
+  p = finger.getModel();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.print("Template "); Serial.print(id); Serial.println(" transferring:");
+      break;
+    default:
+      Serial.print("Unknown error "); Serial.println(p);
+      return;
+  }
+  
+  uint8_t f_buf[512];
+  if (finger.get_template_buffer(512, f_buf) == FINGERPRINT_OK) {
+    for (int k = 0; k < 4; k++) {
+      for (int l = 0; l < 128; l++) {
+        Serial.print(f_buf[(k * 128) + l], HEX);
+        Serial.print(",");
+      }
+      Serial.println("");
+    }
+  }
+}
+
+void write_template_data_to_sensor() {
+  int template_buf_size=512; //usually hobby grade sensors have 512 byte template data, watch datasheet to know the info
+  
+  /*
+  you can manually save the data got from "get_template.ino" example like this
+
+  uint8_t fingerTemplate[512]={0x03,0x0E,....your template data.....};
+  
+  */
+  memset(fingerTemplate, 0xff, 512); //comment this line if you've manually put data to the line above
+  
+  Serial.println("Ready to write template to sensor...");
+  Serial.println("Enter the id to enroll against, i.e id (1 to 127)");
+  int id = 1;
+  if (id == 0) {// ID #0 not allowed, try again!
+    return;
+  }
+  Serial.print("Writing template against ID #"); Serial.println(id);
+
+  if (finger.write_template_to_sensor(template_buf_size,f_buf)) { //telling the sensor to download the template data to it's char buffer from upper computer (this microcontroller's "fingerTemplate" buffer)
+    Serial.println("now writing to sensor...");
+  } else {
+    Serial.println("writing to sensor failed");
+    return;
+  }
+
+  Serial.print("ID "); Serial.println(id);
+  if (finger.storeModel(id) == FINGERPRINT_OK) { //saving the template against the ID you entered or manually set
+    Serial.print("Successfully stored against ID#");Serial.println(id);
+  } else {
+    Serial.println("Storing error");
+    return ;
+  }
 }
